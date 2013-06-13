3c52e6d457726e54fee626b31ead1fc235459bc5
Marc Schachtel <schachtel.marc@gmail.com>
2013-06-13 13:14:34 +0200

    Commenting standard and License information

--- src/main/java/com/parship/propertree/client/propertree.java
+++ src/main/java/com/parship/propertree/client/propertree.java
@@ -1,3 +1,20 @@
+/*
+ * Propertree - A webapplication to administer roperty backed apps
+ * Copyright (C) 2013 PARSHIP GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.parship.propertree.client;
 
 import com.parship.propertree.shared.FieldVerifier;
@@ -19,8 +36,13 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
+ * 
+ * @author marc
+ *
+ * Since 13.06.2013
  */
-public class propertree implements EntryPoint {
+
+public class Propertree implements EntryPoint {
   /**
    * The message displayed to the user when the server cannot be reached or
    * returns an error.
--- src/main/java/com/parship/propertree/shared/FieldVerifier.java
+++ src/main/java/com/parship/propertree/shared/FieldVerifier.java
@@ -1,3 +1,20 @@
+/*
+ * Propertree - A webapplication to administer roperty backed apps
+ * Copyright (C) 2013 PARSHIP GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.parship.propertree.shared;
 
 /**
@@ -19,6 +36,10 @@ package com.parship.propertree.shared;
  * system) cannot be compiled into client side JavaScript. Code that uses native
  * JavaScript (such as Widgets) cannot be run on the server.
  * </p>
+ * 
+ * @author marc
+ *
+ * Since 13.06.2013
  */
 public class FieldVerifier {
 
