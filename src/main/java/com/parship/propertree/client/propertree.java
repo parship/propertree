c816acea9f0aec23d650f4a74028f15d0206c25c
Marc Schachtel <schachtel.marc@gmail.com>
2013-06-13 20:34:30 +0200

    Added first login screen

--- src/main/java/com/parship/propertree/client/propertree.java
+++ src/main/java/com/parship/propertree/client/propertree.java
@@ -17,22 +17,9 @@
 
 package com.parship.propertree.client;
 
-import com.parship.propertree.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.parship.propertree.client.panel.LoginPanelCreator;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -43,18 +30,12 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 
 public class Propertree implements EntryPoint {
-  /**
-   * The message displayed to the user when the server cannot be reached or
-   * returns an error.
-   */
-  private static final String SERVER_ERROR = "An error occurred while "
-      + "attempting to contact the server. Please check your network "
-      + "connection and try again.";
 
   /**
    * This is the entry point method.
    */
   public void onModuleLoad() {
-     // TODO
+     
+      RootPanel.get("mainPanel").add(new LoginPanelCreator().getPanel());
   }
 }
